33600d215c5c8bd8ee250a88acfe60708fe5854d
Sridharan Jayabal <sridharan@docon.co.in>
2021-01-15 23:42:35 +0530

    table created

---
+++ README.md
@@ -0,0 +1,6 @@
+# Table with pagination, sorting, filter using html,css,js
+
+1. install node modules by running yarn or npm i
+2. start the server by running yarn dev or npm run dev
+
+Configurable items are placed at the top of index.js file.
---
+++ index.js
@@ -0,0 +1,167 @@
+(function(){
+
+const headers = ['numericCode', 'capital', 'region', 'population', 'name'], 
+    sortable = ['region', 'population', 'name'], 
+    filterable = ['capital', '', 'population', 'name'], 
+    isPaginated = true
+
+let data = [], references = {}, itemsPerPage = 10, current = 1, totalPages = 0, filteredData = []
+
+const fetchData = async () => {
+    fetch('https://restcountries.eu/rest/v2/all')
+    .then(res => res.json())
+    .then(res => {
+        data = res
+        getReferencesAndHeader()
+        if (isPaginated) {
+            renderTable(res.slice(0,itemsPerPage))
+            createPagers()
+            createPagination(res)
+        } else {
+            renderTable(res)
+        }
+        references['tableHeader'].addEventListener('click', sortTable)
+        addFilters()
+    })
+    .catch(err => {})
+}
+fetchData()
+
+
+const resetValues = (data) => {
+    current = 1
+    if(isPaginated) {
+        renderTable(data.slice(0, itemsPerPage))
+        createPagination(data)
+    } else renderTable(data)
+}
+
+const attachPagerListeners = () => {
+    document.getElementById('pagination').addEventListener('click', e => changePagination(e.target.innerHTML))
+    document.getElementById('prev').addEventListener('click', e => changePagination(current-1))
+    document.getElementById('next').addEventListener('click', e => changePagination(current+1))
+}
+
+const resetInputs = (ignore = '') => {
+    const inputs = document.getElementsByTagName('input')
+    Array.prototype.slice.call(inputs).forEach(element => {
+        if (element.id !== ignore) element.value = null
+    });
+}
+
+const sortTable = (e) => {
+    const key = e.target.innerHTML
+    if (key && sortable.indexOf(key)>-1) {
+        data = data.sort((a,b) => (a[key] > b[key]) ? 1 : ((b[key] > a[key]) ? -1 : 0))
+        resetValues(data)
+        resetInputs()
+        filteredData = []
+    }
+}
+
+const changePagination = (value) => {
+    if (value<1 || value>totalPages) return
+
+    document.querySelector(`[data-value="${current}"]`).classList.remove('active')
+    document.querySelector(`[data-value="${value}"]`).classList.add('active')
+    current = Number(value)
+
+    filteredData.length ?
+    renderTable(filteredData.slice(value * itemsPerPage - itemsPerPage, value * itemsPerPage))
+    :
+    renderTable(data.slice(value * itemsPerPage - itemsPerPage, value * itemsPerPage))
+}
+
+const getReferencesAndHeader = () => {
+    references['table'] = document.getElementById('table')
+    references['tableHeader'] = document.getElementById('tableHeader')
+    references['tableBody'] = document.getElementById('tableBody')
+    references['pagination'] = document.getElementById('pagination')
+    references['pagerButtons'] = document.getElementById('pagerButtons')
+
+    const row = references['tableHeader'].insertRow()
+    headers.forEach(element => {
+        const cell = row.insertCell()
+        cell.appendChild(document.createTextNode(element))
+        sortable.indexOf(element)>-1 ? cell.classList.add('sortable') : ''
+    })
+}
+
+const addFilters = () => {
+    const row = references['tableHeader'].insertRow()
+    headers.forEach(element => {
+        const cell = row.insertCell()
+        if (filterable.indexOf(element)>-1) {
+            const input = document.createElement('input')
+            input.setAttribute('id', element)
+            cell.appendChild(input)
+            input.addEventListener('keyup', filterData)
+        }
+    })
+}
+
+const filterData = (e) => {
+    if (e.target.id && e.target.value) {
+        filteredData = data.filter(el => {
+            return new RegExp(e.target.value, 'i').test(el[e.target.id])
+        })
+        resetValues(filteredData)
+        resetInputs(e.target.id)
+        if (isPaginated) createPagination(filteredData)
+    } else {
+        filteredData = []
+        resetValues(data)
+        resetInputs()
+        if (isPaginated) createPagination(data)
+    }
+}
+  
+const renderTable = (values) => {
+    references['tableBody'].innerHTML = null
+    values.forEach(element => {
+        const row = references['tableBody'].insertRow()
+        headers.forEach(headerKey => {
+            const cell = row.insertCell()
+            cell.appendChild(document.createTextNode(element[headerKey]))
+        })
+    })
+}
+
+const createPagination = (values) => {
+
+    let pageNumbers = []
+    for (let i = 1; i <= Math.ceil(values.length / itemsPerPage); i++) {
+      pageNumbers.push(i)
+    }
+    totalPages = pageNumbers.length
+    const fragment = document.createDocumentFragment()
+
+    pageNumbers.forEach(element => {
+        const span = document.createElement('span')
+        span.innerHTML = element
+        span.dataset.value = element
+        element == current ? span.className = 'active' : ''
+        fragment.appendChild(span)
+    })
+
+    references['pagination'].innerHTML = null
+    references['pagination'].appendChild(fragment)
+
+}
+
+const createPagers = () => {
+    const prev = document.createElement('button')
+    prev.innerHTML = 'Prev'
+    prev.setAttribute('id', 'prev')
+    references['pagerButtons'].appendChild(prev)
+
+    const next = document.createElement('button')
+    next.innerHTML = 'Next'
+    next.setAttribute('id', 'next')
+    references['pagerButtons'].appendChild(next)
+
+    attachPagerListeners()
+
+}
+
+})()
